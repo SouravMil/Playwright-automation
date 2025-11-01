f00efd94ceb5d5a30a3f4a98b63cc435c854c64c
SouravMil <souravmilalts@gmail.com>
2025-11-01 23:16:42 +0530

    practiceProject3

--- tests/practiceProject2.spec.js
+++ tests/practiceProject2.spec.js
@@ -8,18 +8,34 @@ test("Form Automation & Validation", async ({ page }) => {
   const genderList = page.locator("#genterWrapper .custom-control");
   for (let i = 0; i < (await genderList.count()); i++) {
     const genderLabel = await genderList
-      .nth(i).locator(".custom-control-label")
+      .nth(i)
+      .locator(".custom-control-label")
       .textContent();
     if (genderLabel === "Male") {
       await genderList.nth(i).locator(".custom-control-label").click();
     }
   }
-  await page.fill('#userNumber','987654321');
-  const calendar = page.locator('#dateOfBirthInput');
-  await calendar.click();
-  const calendarPop = calendar.locator('.react-datepicker__month-container');
-  await calendarPop.selectOption('.react-datepicker__month-select','August');
-  await calendarPop.selectOption('.react-datepicker__year-select','1947');
-  await calendarPop.getByText('15').click();
-  expect(calendar).toHaveValue('15 Aug 1947');
+  await page.fill("#userNumber", "987654321");
+  // const calendarPlaceholder = page.locator('#dateOfBirthInput');
+  // await calendarPlaceholder.click();
+  // const calendarPop = calendarPlaceholder.locator('.react-datepicker__month-container');//// looks good
+  // const monthDropdown = calendarPop.locator('.react-datepicker__month-dropdown-container');
+  // await monthDropdown.selectOption('.react-datepicker__month-select','August');
+  // const yearDropdown = calendarPop.locator('.react-datepicker__year-dropdown-container');
+  // await yearDropdown.selectOption('.react-datepicker__year-select','1947');
+  // await calendarPop.locator('.react-datepicker__month').getByText('15').click();
+  // expect(calendar).toHaveValue('15 Aug 1947');
+  const hobbies = page
+    .locator("#hobbiesWrapper")
+    .locator(".col-md-9")
+    .nth(5)
+    .locator(".custom-checkbox");
+  for (let j = 0; j < (await hobbies.count()); j++) 
+    {
+      const hobbyText = await hobbies.nth(i).locator('custom-control-label').textContent();
+      if(hobbyText === 'Music')
+      {
+        await hobbies.nth(i).locator('.custom-control-input').click();
+      }
+    }
 });
